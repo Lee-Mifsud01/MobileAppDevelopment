147af8372daa88021be2c2ba1705ca1c2727e174
Lyona <lyonamanche@gmail.com>
2025-04-14 18:35:57 +0200

    Clubs page
    clubs page, html, css & ts added

--- inb-mcast/src/app/tab9/tab9.page.ts
+++ inb-mcast/src/app/tab9/tab9.page.ts
@@ -14,5 +14,17 @@ export class Tab9Page {
   back() {
     this.functionService.back();
   }
-
+  clubs = [
+    { name: 'Sports Club', icon: 'football-outline', description: 'Stay active and healthy' },
+    { name: 'Gaming Club', icon: 'game-controller-outline', description: 'Play. Compete. Connect.' },
+    { name: 'Dance Club', icon: 'musical-notes-outline', description: 'Move with the rhythm' },
+    { name: 'Photography Club', icon: 'camera-outline', description: 'Capture moments creatively' },
+    { name: 'Music Club', icon: 'headset-outline', description: 'Play, sing, vibe together' },
+    { name: 'Tech Club', icon: 'hardware-chip-outline', description: 'Innovate & create with tech' },
+    { name: 'Drama Club', icon: 'videocam-outline', description: 'Lights, Camera, Action!' },
+    { name: 'Literature Club', icon: 'book-outline', description: 'For the love of books' },
+    { name: 'Adventure Club', icon: 'compass-outline', description: 'Explore. Hike. Discover.' },
+    { name: 'Art Club', icon: 'color-palette-outline', description: 'Unleash your creativity' }
+  ];
+  
 }
