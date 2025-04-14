705dd5f79edef3cbe46704771a0a03073b389c49
Lyona <lyonamanche@gmail.com>
2025-04-14 21:01:09 +0200

    Well-being page
    Well being page html, ts & css added with alert

--- inb-mcast/src/app/tab10/tab10.page.ts
+++ inb-mcast/src/app/tab10/tab10.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FunctionsService } from '../services/functions.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab10',
@@ -9,10 +10,60 @@ import { FunctionsService } from '../services/functions.service';
 })
 export class Tab10Page {
 
-  constructor(private functionService: FunctionsService) {}
+  constructor(
+    private functionService: FunctionsService,
+    private alertController: AlertController
+  ) {}
 
   back() {
     this.functionService.back();
   }
 
+  async openServiceInfo(service: string) {
+    let header = '';
+    let message = '';
+
+    switch(service) {
+      case 'therapy':
+        header = 'Therapy Services';
+        message =
+          'Professional therapy services to support your mental health and well-being.\n\n' +
+          'Phone: +356 2398 7188 \n' +
+          'Email: counsellors@mcast.edu.mt';
+        break;
+
+      case 'addiction':
+        header = 'Addiction Help';
+        message =
+          'Resources and counseling for addiction recovery and support.\n\n' +
+          'Phone: +356 2398 7188\n' +
+          'Email: substanceissues@mcast.edu.mt';
+        break;
+
+      case 'mental-health':
+        header = 'Mental Health Support';
+        message =
+          'Talk to professionals about anxiety, stress, or other mental health concerns.\n\n' +
+          'Phone: +356 2398 7188 \n' +
+          'Email: counsellors@mcast.edu.mt';
+        break;
+
+      case 'nutrition':
+        header = 'Nutritional Advice';
+        message =
+          'Get advice from qualified nutritionists for a healthier lifestyle.\n\n' +
+          'Phone: +356 2398 7188 \n' +
+          'Email: nutrition@mcast.edu.mt';
+        break;
+    }
+
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['Close'],
+      cssClass: 'custom-alert',
+    });
+
+    await alert.present();
+  }
 }
