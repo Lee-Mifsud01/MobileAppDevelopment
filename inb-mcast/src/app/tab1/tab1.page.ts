e9b01304e58b14aa14229d2bdfe95af8b344f5d9
Lyona <lyonamanche@gmail.com>
2025-04-14 21:47:54 +0200

    Dark mode button
    I added the dark mode toggle functionality in the footer(still need to work on it, not done)

--- inb-mcast/src/app/tab1/tab1.page.ts
+++ inb-mcast/src/app/tab1/tab1.page.ts
@@ -23,4 +23,19 @@ export class Tab1Page implements OnInit {
     const diff = this.targetDate.getTime() - today.getTime();
     this.daysLeft = Math.ceil(diff / (1000 * 60 * 60 * 24));
   }
+
+  isDarkMode: boolean = false;
+
+toggleDarkMode() {
+  this.isDarkMode = !this.isDarkMode;
+
+  if (this.isDarkMode) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+
+  localStorage.setItem('dark-mode', this.isDarkMode.toString());
+}
+
 }
