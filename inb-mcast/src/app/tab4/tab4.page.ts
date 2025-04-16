819432944cfa23c2a0b97004324461e38ac3425a
Lyona <lyonamanche@gmail.com>
2025-04-16 14:44:45 +0200

    News Updates More info
    More info button, added links to the official mcast website.

--- inb-mcast/src/app/tab4/tab4.page.ts
+++ inb-mcast/src/app/tab4/tab4.page.ts
@@ -10,21 +10,25 @@ export class Tab4Page {
 
   constructor() {}
 
-
   news = [
     {
       title: "Fresher’s week – Gozo Campus (11th October)",
-      image: 'assets/images/gozo.jpg'
+      image: 'assets/images/gozo.jpg',
+      link: 'https://mcast.edu.mt/freshers-week-2024/'
     },
     {
       title: "Fresher’s week – Institute for the Creative Arts",
-      image: 'assets/images/ica.jpg'
+      image: 'assets/images/ica.jpg',
+      link: 'https://mcast.edu.mt/freshers-week-2024/'
     },
     {
       title: "Fresher’s week – (7th, 8th, 9th October)",
-      image: 'assets/images/main.jpg'
+      image: 'assets/images/main.jpg',
+      link: 'https://mcast.edu.mt/freshers-week-2024/'
     }
   ];
-  
 
+  openLink(url: string) {
+    window.open(url, '_blank'); // opens in new tab
+  }
 }
