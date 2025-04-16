d3745638f1713cbdb548bf6d2055554050713568
youThink2 <daniel.mallia.g53483@mcast.edu.mt>
2025-04-16 16:32:46 +0200

    Tab8 and 7
    Food menu finalised to include the dates of the current week. Downloadable handbook pdf up

--- inb-mcast/src/app/tab8/tab8.page.ts
+++ inb-mcast/src/app/tab8/tab8.page.ts
@@ -10,5 +10,49 @@ export class Tab8Page {
 
   constructor() {}
 
+  menu = [
+    {
+      name: 'Monday',
+      soup: 'Soup of Mushroom & Chicken',
+      main: 'Chicken Drumsticks with Garlic & Fresh Herbs Potato',
+      vegetable: 'Cauliflower Cheese (V)(GF)',
+      pasta: 'Sauteed Vegetables & Fresh Herbs'
+    },
+    {
+      name: 'Tuesday',
+      soup: 'Lentil & Vegetables (V)(GF)',
+      main: 'Cottage Pie',
+      vegetable: 'French Beans (V)(GF)',
+      pasta: 'Cream Chicken & Spinach (V)(GF)'
+    },
+    {
+      name: 'Wednesday',
+      soup: 'Spicy Potato (V)',
+      main: 'Grilled Maltese Sausage in Onion Gravy with Mash',
+      vegetable: 'Peas & Carrots (V)(GF)',
+      pasta: 'Mac & Cheese'
+    },
+    {
+      name: 'Thursday',
+      soup: 'Cream of Sweetcorn (GF)',
+      main: 'Chicken Thighs Mushroom Sauce Potato Wedges',
+      vegetable: 'Mixed Vegetables (V)(GF)',
+      pasta: 'Egg Fried Rice & Vegetable'
+    }
+  ];
+
+  getFormattedDate(index: number): string {
+
+    
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+
+    const monday = new Date(today);
+    const diffToMonday = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
+    monday.setDate(today.getDate() + diffToMonday + index);
+
+    const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long' };
+    return monday.toLocaleDateString('en-GB', options);
+  }
 
 }
