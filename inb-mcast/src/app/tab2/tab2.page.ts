f5ea777fdd050b60e8b0628fa4515b5f51fd109e
mattsimi <147156916+mattsimi@users.noreply.github.com>
2025-04-16 16:52:05 +0200

    Changed layouts, added images and added functionality

--- inb-mcast/src/app/tab2/tab2.page.ts
+++ inb-mcast/src/app/tab2/tab2.page.ts
@@ -2,13 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
   standalone: false,
 })
 export class Tab2Page {
+  searchTerm: string = '';
+  lecturers: string[] = [
+    'Mr. Dominic Tringali',
+    'Mr. Nico Conti',
+    'Ms. Nadia Haber',
+    'Ms. Claire Vella'
+  ];
+  filtered: string[] = [];
 
-  constructor() {}
+  constructor() {
+    this.filtered = this.lecturers;
+  }
 
-
+  filterLecturers() {
+    const term = this.searchTerm.toLowerCase();
+    this.filtered = this.lecturers.filter(lecturer =>
+      lecturer.toLowerCase().includes(term)
+    );
+  }
 }
