79167987c65f502e20f6c068cad06ba5cffbb180
Lee-Mifsud01 <128689240+Lee-Mifsud01@users.noreply.github.com>
2025-04-16 23:02:32 +0200

    added the switch map feature to have the ability to switch from a cartoony map to a realistic one

--- inb-mcast/src/app/tab12/tab12.page.ts
+++ inb-mcast/src/app/tab12/tab12.page.ts
@@ -8,6 +8,14 @@ import { Component} from '@angular/core';
 })
 export class Tab12Page {
 
+  map1: string = 'assets/images/map.png';
+  map2: string = 'assets/images/map2.png';
   
+  currentMap: string = this.map1;
+
+  switchMap(){
+    this.currentMap = this.currentMap === this.map1 ? this.map2 : this.map1;
+  }
+
   constructor() {}
 }
